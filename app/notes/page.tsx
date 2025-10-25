720797ebab324b0915e0801b68dbb52db685c24b
Vasyl Medulych <linkinmvv@gmail.com>
2025-10-25 11:56:04 +0300

    Initial commit

---
+++ app/notes/page.tsx
@@ -0,0 +1,21 @@
+import { fetchNotes } from "@/lib/api";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import NotesClient from "./Notes.client";
+
+export default async function Notes() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["notes", "", 1],
+    queryFn: () => fetchNotes("", 1),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient />
+    </HydrationBoundary>
+  );
+}
