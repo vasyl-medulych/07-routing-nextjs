2932ea2d62d50634d2a77fb2d5edce11990c2241
Vasyl Linkin <linkinmvv@gmail.com>
2025-10-30 23:45:53 +0200

    fix task

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -3,17 +3,15 @@ import css from "./LayoutNotes.module.css";
 interface Props {
   children: React.ReactNode;
   sidebar: React.ReactNode;
-  modal: React.ReactNode;
 }
 
-const NotesLayout = ({ children, sidebar, modal }: Props) => {
+const NotesLayout = ({ children, sidebar }: Props) => {
   return (
     <>
       <section className={css.container}>
         <aside className={css.sidebar}>{sidebar}</aside>
         <div className={css.notesWrapper}>{children}</div>
       </section>
-      {modal}
     </>
   );
 };
